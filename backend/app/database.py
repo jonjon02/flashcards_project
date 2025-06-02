8d6a67fab5dcf3a0e32b63ee30e3af48a24dcc53
jbruch <bruch@jacob.de>
2025-06-02 13:11:00 +0200

    updated DbWrapper

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,95 +1,46 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_self.engine
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from models import *
 from datetime import datetime
 
-# user = "jbruch"
-# pwd = "WMrtrm#RZ7G"
-# host = "localhost:5431"
 class DbWrapper:
-    __init__(self, dbuser: str, pwd: str, host: str):
-        dbuser = self.dbuser
-        pwd = self.pwd
-        host = self.host
+    def __init__(self, dbuser: str, pwd: str, host: str, database: str):
+        self.self.engine = create_self.engine(
+            f'postgresql+psycopg2://{dbuser}:{pwd}@{host}/{database}',
+            echo=False
+        )
 
-<<<<<<< HEAD
-def get_user(id: int):
-    with engine.connect() as conn:
-        result = conn.execute(select(User).where(User.user_id == id).limit(1))
-        return result.fetchall()
-    
-def get_deck(id: int):
-    with Session(engine) as session:
-        deck = session.get(Deck, id)
-        return deck
-    
-def get_decks(user_Id: int):
-    with Session(engine) as session:
-        result = select(Deck).where(user_Id==user_Id)
-        decks = session.scalars(result).all()
-        return decks
-    
-def get_single_card(id: int):
-    with Session(engine) as session:
-        card = session.get(Card, id)
-        return card
-
-def get_cards(deck_id: int):
-    with Session(engine) as session:
-        result = select(Card).where(Card.deck_id_fk==deck_id)
-        cards = session.scalars(result).all()
-        return cards
-    
-def insert_card(deck_id: int, question: str, answer: str):
-    with Session(engine) as session:
-        session.add(Card(deck_id_fk=deck_id, question=question, answer=answer, date_created=datetime.now()))
-        session.commit()
-        return Card.card_id
-    
-def insert_deck(user_id: int, deck_name: str, deck_descr: str):
-    with Session(engine) as session:
-        session.add(Deck(user_id_fk=user_id, deck_name=deck_name, deck_description=deck_descr, date_created=datetime.now()))
-        session.commit()
-
-for d in get_decks(2):
-    print(d.deck_name)
-
-get_cards(2)
-=======
-    engine = create_engine(f'postgresql+psycopg2://{dbuser}:{pwd}@{host}/flashcards', echo=False)
-
-    def get_user(id: int):
-        with engine.connect() as conn:
+    def get_user(self, id: int):
+        with self.engine.connect() as conn:
             result = conn.execute(select(User).where(User.user_id == id).limit(1))
             return result.fetchall()
         
-    def get_deck(id: int):
-        with Session(engine) as session:
+    def get_deck(self, id: int):
+        with Session(self.engine) as session:
             deck = session.get(Deck, id)
             return deck
         
-    def get_single_card(id: int):
-        with Session(engine) as session:
+    def get_single_card(self, id: int):
+        with Session(self.engine) as session:
             card = session.get(Card, id)
             return card
 
-    def get_cards(deck_id: int):
-        with Session(engine) as session:
+    def get_cards(self, deck_id: int):
+        with Session(self.engine) as session:
             result = select(Card).where(Card.deck_id_fk==deck_id)
             cards = session.scalars(result).all()
             return cards
         
-    def insert_card(deck_id: int, question: str, answer: str):
-        with Session(engine) as session:
+    def insert_card(self, deck_id: int, question: str, answer: str):
+        with Session(self.engine) as session:
             session.add(Card(deck_id_fk=deck_id, question=question, answer=answer, date_created=datetime.now()))
             session.commit()
             return Card.card_id
         
-    def insert_deck(user_id: int, deck_name: str, deck_descr: str):
-        with Session(engine) as session:
+    def insert_deck(self, user_id: int, deck_name: str, deck_descr: str):
+        with Session(self.engine) as session:
             session.add(Deck(user_id_fk=user_id, deck_name=deck_name, deck_description=deck_descr, date_created=datetime.now()))
             session.commit()
             return Deck.deck_id
->>>>>>> 33e0c3498f5e8fb07bcf162c55ea04c4b4225d82
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-<<<<<<< HEAD
-from database import *
-
-app = FastAPI()
-
-@app.get("/decks/{user_id}")
-def get_decks(user_id: int):
-    with Session(engine) as session:
-        result = select(Deck).where(user_id==user_id)
-        decks = session.scalars(result).all()
-        return decks
-    
-        
-=======
 from database import DbWrapper
 
-
-# user = "jbruch"
-# pwd = "WMrtrm#RZ7G"
-# host = "localhost:5431"
-
-db = DbWrapper('jbruch', 'WMrtrm#RZ7G', 'localhost:5431')
+db = DbWrapper('jbruch', 'WMrtrm#RZ7G', 'localhost:5431', 'flashcards')
 
 app = FastAPI()
 
@@ -29,14 +10,3 @@ app = FastAPI()
 def get_deck(deck_id):
     deck = db.get_deck(deck_id)
         
-
-# @app.post("/library/")
-# def create_deck(deck: deckCreate):
-#     if m
-#     deck_database.append(deck(deck_id=deck.deck_id, deck_name=deck.deck_name, deck_description=deck.deck_description, deck_owner=deck.deck_owner))
-#     return {"deck_id = ", deck.deck_id}
-
-# @app.get("/library/")
-# def get_decks():
-#     return deck_database
->>>>>>> 33e0c3498f5e8fb07bcf162c55ea04c4b4225d82
