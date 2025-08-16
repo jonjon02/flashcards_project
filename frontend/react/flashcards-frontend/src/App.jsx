9399ca5d1226c67194bf065c2d3e4c717b929b06
jbruch <bruch@jacob.de>
2025-08-16 18:45:39 +0200

    added font end dev

---
+++ frontend/react/flashcards-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Navbar from './components/navbar';
+
+export default function App() {
+  return (
+    <>    
+    <div>
+       <Navbar />
+    </div>
+    <div style={{textAlign: 'center'}}>
+      <h1>Flashcards</h1>
+    </div>
+    </>
+  );
+}
+
+  // <div> 
+  //    
+  //   </div>
---
+++ frontend/react/flashcards-frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Library</li>
+                <li>About</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar
