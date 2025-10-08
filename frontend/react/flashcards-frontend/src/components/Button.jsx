f6431493cfa4277fdf6eb439e7d20110fae47261
jbruch <bruch@jacob.de>
2025-10-08 08:17:24 +0200

    button component

--- frontend/react/flashcards-frontend/src/components/Button.jsx
+++ frontend/react/flashcards-frontend/src/components/Button.jsx
@@ -1,7 +1,10 @@
 import './Button.css'
+import { useState } from 'react'
 import { AlignStartHorizontal, Search } from 'lucide-react'
 
 export function MyButton(props) {
+
+    const [answer, setAnswer] = useState(null)
 
     async function getData() {
         const url = "http://127.0.0.1:8000/library/deck/card/3"
@@ -10,24 +13,32 @@ export function MyButton(props) {
             if (!response) {
                 throw new Error(`Response status ${response.status}`)
             }
-            console.log("test2")
+   
+            const resultSet = await response.json()
+            const resultReadable = resultSet.question + "\n" + resultSet.answer + "\n" + resultSet.difficulty + "\n" + resultSet.date_created
+            setAnswer(resultReadable)
             
-            const result = await response.json()
-            console.log(result.answer)
+            console.log(resultReadable)
+            alert(resultReadable)
             
             }
-        
+    
             catch {
             console.error()
         }
-    
-        return result.answer
+        return resultSet
     }
 
     return (
+        <>
         <button 
             onClick={getData} className='button'>
             Search
         </button>
-    )
-} 
+        <div>
+        <p className="api-result">
+            Antwort: {answer}
+        </p>
+        </div>
+        </>
+)} 
